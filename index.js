9bfbd08b1860dccaa90261ecda6ff4ed306421bf
aguerreroperez1973 <aguerreroperez1973@penguin>
2024-03-17 11:19:40 -0300

    version con trycatch y middleware

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+
+//////////////////
+app.use((req, res, next) => {
+    console.log('Request Type:', req.method, '| Time:', Date(),'| Request URL:', req.originalUrl )
+    next();
+  });  
+/////////////////
+
 app.listen(3000, console.log('Server ON'))
 
 app.get("/home", (req, res) => {
@@ -9,23 +17,41 @@ app.get("/home", (req, res) => {
 const { getJoyas, formatHATEOAS, getJoyasFiltro, getJoya } = require('./models/joyasModel.js')
 
 app.get('/joyas', async (req, res) => {
-    const queryStrings = req.query
-    const joyas = await getJoyas(queryStrings)
-    const HATEOAS = await formatHATEOAS(joyas)
-    //res.json(joyas)
-    res.json(HATEOAS);
+    try {
+        const queryStrings = req.query
+        const joyas = await getJoyas(queryStrings)
+        const HATEOAS = await formatHATEOAS(joyas)
+        //res.json(joyas)
+        //res.json(HATEOAS);
+        res.status(200).json(HATEOAS);
+
+    }catch(error){
+        res.status(500).json({error});
+    }
 })
 
 app.get('/joyas/filtros', async (req, res) => {
-    const queryStrings = req.query
-    const joyas = await getJoyasFiltro(queryStrings)
-    res.json(joyas)
-    })
+    try{ 
+        const queryStrings = req.query
+        const joyas = await getJoyasFiltro(queryStrings)
+        //res.json(joyas)
+        res.status(200).json(joyas);
+
+    }catch(error){
+        res.status(500).json({error});
+    }
+})
 
 app.get('/joyas/joya/:id', async (req, res) => {
-    const { id } = req.params
-    const joya = await getJoya(id)
-    res.json(joya)
+    try{
+        const { id } = req.params
+        const joya = await getJoya(id)
+        //res.json(joya)
+        res.status(200).json(joya);
+
+    }catch(error){
+        res.status(500).json({error});
+    }
     })
 
 //////// rutas desconocidas //////////////////////////////
--- models/joyasModel.js
+++ models/joyasModel.js
@@ -2,54 +2,82 @@ const format = require('pg-format');
 const { pool } = require('../controller/controller.js')
 
 const getJoyas = async ({ limits = 10, page = 1, order_by = "id_ASC" }) => {
-    
-    let consulta = "SELECT * FROM inventario order by %s %s LIMIT %s OFFSET %s"
 
-    const [campo, direccion] = order_by.split("_");
-    const offset = Math.abs((page - 1) * limits)
+    try {
+        let consulta = "SELECT * FROM inventario order by %s %s LIMIT %s OFFSET %s"
 
-    const formattedQuery = format(consulta, campo, direccion, limits, offset);
-    const { rows: joyas } = await pool.query(formattedQuery)
-    return joyas
+        const [campo, direccion] = order_by.split("_");
+        const offset = Math.abs((page - 1) * limits)
+
+        const formattedQuery = format(consulta, campo, direccion, limits, offset);
+        const { rows: joyas } = await pool.query(formattedQuery)
+        return joyas
+
+    }catch(error){
+        console.error(error.message)
+        throw error.message;
+    }
     }
 
 const getJoyasFiltro = async ({ precio_min, precio_max, categoria, metal }) => {
-    let filtros = []
-    if (precio_min) filtros.push(`precio >= ${precio_min}`)
-    if (precio_max) filtros.push(`precio <= ${precio_max}`)
-    if (categoria) filtros.push(`categoria = '${categoria}'`)
-    if (metal) filtros.push(`metal = '${metal}'`)
+    try{
+        let filtros = []
+        if (precio_min) filtros.push(`precio >= ${precio_min}`)
+        if (precio_max) filtros.push(`precio <= ${precio_max}`)
+        if (categoria) filtros.push(`categoria = '${categoria}'`)
+        if (metal) filtros.push(`metal = '${metal}'`)
 
-    let consulta = "SELECT * FROM inventario"
-    if (filtros.length > 0) {
-        filtros = filtros.join(" AND ")
-        consulta += ` WHERE ${filtros}`
+        let consulta = "SELECT *, SUM(stock) as totalStock FROM inventario"
+        if (filtros.length > 0) {
+            filtros = filtros.join(" AND ")
+            consulta += ` WHERE ${filtros}`
+        }
+            const { rows: joyas } = await pool.query(consulta)
+        return joyas
+
+    }catch(error){
+        console.error(error.message)
+        throw error.message;
     }
-        const { rows: joyas } = await pool.query(consulta)
-    return joyas
     }
 
 const getJoya = async (id) => {
-        
-    let consulta = "SELECT * FROM inventario WHERE id = $1"
+    try {
+        let consulta = "SELECT * FROM inventario WHERE id = $1"
         const values = [id]
         const { rows } = await pool.query(consulta, values)
     return rows
+    } catch(error){
+        console.error(error.message)
+        throw error.message;
+    } 
     }
 
 const formatHATEOAS = (joyas) => {
-    const results = joyas.map((m) => {
-    return {
-        name: m.nombre,
-        href: `http://localhost:3000/joyas/joya/${m.id}`,
+
+   const totalStock = joyas.reduce((anterior, actual) => anterior + actual.stock, 0);
+
+    try{
+        const results = joyas.map((m) => { 
+            return {
+                name: m.nombre,
+                href: `http://localhost:3000/joyas/joya/${m.id}`,
+            }
+            
+            }).slice(0, joyas.length )
+                const totalJoyas = joyas.length
+                const HATEOAS = {
+                    totalJoyas,
+                    totalStock,
+                    results,
+                }
+                return HATEOAS
+
+        }catch(error){
+        console.error(error.message)
+        throw error.message;
     }
-    }).slice(0, joyas.length)
-        const total = joyas.length
-        const HATEOAS = {
-            total,
-            results,
-        }
-        return HATEOAS
+
     }
 
 ////// Exportamos las funciónes
